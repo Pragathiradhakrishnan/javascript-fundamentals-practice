1d861256b2afd6a3dcbd8385128c296ba93f0edf
Pragathi R <pragathiradhakrishnan673@gmail.com>
2026-04-09 06:19:33 +0530

    loops-practice.js

---
+++ loops-practice.js
@@ -0,0 +1,22 @@
+//for loop
+
+for(let i=1;i<=10;i++){
+for(let i=10;i>0;i--){
+    console.log(i+". for loop");  
+}
+
+//while loop
+let i=10;
+while(i>0){
+    console.log(i+". while loop");
+    i--;
+    
+}
+ 
+//do while
+let i=0;
+do{
+    console.log(i+". do while")
+    i++
+}while(i<=10){
+}
