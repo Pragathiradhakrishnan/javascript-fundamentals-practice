fa248f150d1bf70d83cb40e6e64d98bf224c1f2b
Pragathi R <pragathiradhakrishnan673@gmail.com>
2026-04-09 06:18:01 +0530

    switch-food-menu.js

---
+++ switch-food-menu.js
@@ -0,0 +1,29 @@
+let choice=prompt("Enter your choice fro food😋 from 1 to 7:");
+switch (Number(choice)) {
+    case 1:
+        console.log("You selected Pizza!🍕");
+        break;
+    case 2:
+        console.log("You selected Burger!🍔");
+        break;
+    case 3:
+        console.log("You selected Pasta!🍝");
+        break;
+    case 4:
+        console.log("You selected Noodles!🍜");
+        break;
+    case 5:
+        console.log("You selected Rice!🍛");
+        break;
+    case 6:
+        console.log("You selected Salad!🥗");
+        break;
+    case 7:
+        console.log("You selected Juice!🥤");
+        break;
+
+    default:
+        console.log("Invalid Choice!");
+        
+        break;
+}
